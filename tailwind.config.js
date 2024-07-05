c203337d4c6e9006deae148b79a439d569192ce4
Julio Cesar Calderón García <juliansfreelance@gmail.com>
2024-07-05 10:02:19 -0500

    Slide 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,17 +10,32 @@ module.exports = {
       colors: {
         text: {
           '500': '#6d6d6d',
+          '600': '#820053'
         },
         menu: {
           '100': '#f0ece9',
           '200': '#bfb6ad',
         },
+        introduccion: {
+          '100': '#DDD7D1'
+        },
+        button: {
+          'text': '#F8971D',
+          'bg1': '#DFE0E4',
+          'bg2': '#FFCF01',
+          'bg3': '#F8971D',
+          'bg4': '#FFF11C'
+        }
+      },
+      dropShadow: {
+        'button': '2px 2px 0px rgba(255, 255, 255, 0.8)',
+        'pop': '1px 2px 2px rgba(0, 0, 0, 0.2)',
       },
       aspectRatio: {
         'ipad': '4 / 3',
       },
       backgroundImage: {
-        'fondo': "url('../images/fondo.jpg')",
+        'fondo': "url('../images/fondo.png')",
       },
     },
   },
