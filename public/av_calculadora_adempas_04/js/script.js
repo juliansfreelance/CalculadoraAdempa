6f42a3406ce2ac6bf9d93117b16cc0cdeb662a91
Julio Cesar Calderón García <juliansfreelance@gmail.com>
2024-07-05 17:35:24 -0500

    Slide 04 - Metodología

---
+++ public/av_calculadora_adempas_04/js/script.js
@@ -0,0 +1,27 @@
+"use strict";
+let veeva = {};
+
+function loadConfig() {
+   return fetch('js/config.json')
+      .then(response => response.json())
+      .then(data => {
+         veeva = data;
+      })
+      .catch(error => {
+         console.error('Error al cargar la configuración:', error);
+      });
+}
+
+function jumptoSlide(slide) {
+   if (typeof veeva !== 'undefined' && veeva.gotoSlide) {
+      document.location = `veeva:gotoSlide(${veeva.zipName}${slide}.zip,${veeva.presentationCode})`;
+   } else {
+      document.location = `/public/${veeva.zipName}${slide}/${veeva.zipName}${slide}.html`;
+   }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+   loadConfig().then(() => {
+      console.log(`LoadConfig Ready Slide ${veeva.zipName}${veeva.slide}`);
+   });
+});
