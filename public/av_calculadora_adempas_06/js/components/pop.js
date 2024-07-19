0a2bd5f7e1ad269c30bdd96db8c0de3995a22924
Julio Cesar Calderón García <juliansfreelance@gmail.com>
2024-07-19 03:19:59 -0500

    code clean slide6

--- public/av_calculadora_adempas_06/js/components/pop.js
+++ public/av_calculadora_adempas_06/js/components/pop.js
@@ -36,9 +36,9 @@ class customPop extends HTMLElement{
       document.addEventListener('configLoaded', () => {
          const { type } = this;
          const terapia = {
-            monoterapia: veeva.monoterapias,
-            biterapia: veeva.terapiasDobles,
-            triterapia: veeva.terapiasTripes,
+            monoterapia: veeva.calculadora.tecnologias.monoterapias,
+            biterapia: veeva.calculadora.tecnologias.terapiasDobles,
+            triterapia: veeva.calculadora.tecnologias.terapiasTripes,
          }[type] || null;
 
          this.innerHTML = `
@@ -62,7 +62,7 @@ class customPop extends HTMLElement{
                <div class="pop-label">
                   <div>
                      <h4>${terapia.title}</h4>
-                     <button onclick="javascript:popDown('${this.type}')" class="text-red-600 shadow-md rounded-full bg-white h-fit">
+                     <button onclick="javascript:slideSeis.popDown('${this.type}')" class="text-red-600 shadow-md rounded-full bg-white h-fit">
                         <svg class="size-7" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
                            <path fill-rule="evenodd"
                               d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25Zm3 10.5a.75.75 0 0 0 0-1.5H9a.75.75 0 0 0 0 1.5h6Z"
--- public/av_calculadora_adempas_06/js/script.js
+++ public/av_calculadora_adempas_06/js/script.js
@@ -1,57 +1,74 @@
 "use strict";
+/**
+ * Lab: Bayer
+ * Agency: ÜlaIdeas
+ * Created by: Julio Calderón
+ * Developed By: Julio Calderón
+ * Modified By:
+ */
 let veeva = {};
 
-function loadConfig() {
-   return fetch('js/config.json')
-      .then(response => response.json())
-      .then(data => {
-         veeva = data;
+let slideSeis = {
+
+   ini: async function () {
+      const calculadoraData = localStorage.getItem('calculadora');
+      if (calculadoraData) {
+         veeva.calculadora = await JSON.parse(calculadoraData);
+         console.log(veeva);
          document.dispatchEvent(new Event('configLoaded'));
-      })
-      .catch(error => {
-         console.error('Error al cargar la configuración:', error);
+      }
+   },
+
+   loadConfig: function() {
+      return fetch('js/config.json').then(response => response.json()).then(data => {
+         veeva = data;
+      }).catch(error => {
+            console.error('Error al cargar la configuración:', error);
       });
-}
+   },
 
-function jumpToSlide(slide) {
-   slide === '02' ? localStorage.setItem('instrucciones', true) : localStorage.removeItem('instrucciones');
-   if (typeof veeva !== 'undefined' && veeva.gotoSlide) {
-      document.location = `veeva:gotoSlide(${veeva.zipName}${slide}.zip,${veeva.presentationCode})`;
-   } else {
-      document.location = `/public/${veeva.zipName}${slide}/${veeva.zipName}${slide}.html`;
+   jumpToSlide: function(slide) {
+      slide === '02' ? localStorage.setItem('instrucciones', true) : localStorage.removeItem('instrucciones');
+      if (typeof veeva !== 'undefined' && veeva.gotoSlide) {
+         document.location = `veeva:gotoSlide(${veeva.zipName}${slide}.zip,${veeva.presentationCode})`;
+      } else {
+         document.location = `/public/${veeva.zipName}${slide}/${veeva.zipName}${slide}.html`;
+      }
+   },
+
+   popUp: function(pop) {
+      const customPop = document.querySelector(`custom-pop[type="${pop}"]`);
+      if (customPop) {
+         customPop.classList.remove('hidden', 'pop-animate-down');
+         customPop.classList.add('flex', 'pop-animate-up');
+      } else {
+         console.error(`No se encontró ningún elemento <custom-pop> con type="${pop}".`);
+      }
+   },
+
+   popDown: function(pop) {
+      const customPop = document.querySelector(`custom-pop[type="${pop}"]`);
+      if (customPop) {
+         customPop.classList.remove('flex', 'pop-animate-up');
+         customPop.classList.add('pop-animate-down');
+         setTimeout(() => {
+            customPop.classList.add('hidden')
+         }, 600);
+      } else {
+         console.error(`No se encontró ningún elemento <custom-pop> con type="${pop}".`);
+      }
+   },
+
+   validarForm: function() {
+      console.log('Validacion slide 06 pendiente');
+      slideSeis.jumpToSlide('07');
    }
-}
+};
 
-function popUp(pop) {
-   const customPop = document.querySelector(`custom-pop[type="${pop}"]`);
-   if (customPop) {
-      customPop.classList.remove('hidden', 'pop-animate-down');
-      customPop.classList.add('block', 'pop-animate-up');
-   } else {
-      console.error(`No se encontró ningún elemento <custom-pop> con type="${pop}".`);
-   }
-}
-
-function popDown(pop) {
-   const customPop = document.querySelector(`custom-pop[type="${pop}"]`);
-   if (customPop) {
-      customPop.classList.remove('block', 'pop-animate-up');
-      customPop.classList.add('pop-animate-down');
-      setTimeout(() => {
-         customPop.classList.add('hidden')
-      }, 600);
-   } else {
-      console.error(`No se encontró ningún elemento <custom-pop> con type="${pop}".`);
-   }
-}
-
-function validarForm() {
-   console.log('Validacion slide 06 pendiente');
-   jumpToSlide('07');
-}
 
 document.addEventListener('DOMContentLoaded', function () {
-   loadConfig().then(() => {
+   slideSeis.loadConfig().then(() => {
       console.log(`LoadConfig Ready Slide ${veeva.zipName}${veeva.slide}`);
+      slideSeis.ini();
    });
 });
