f34da115774c328496bc45b8a31720bd6a2484ef
Julio Cesar Calderón García <juliansfreelance@gmail.com>
2024-08-29 19:30:02 -0500

    ajustebase para nuevo slide procedimientos 2

---
+++ public/av_calculadora_adempas_13/js/script.js
@@ -0,0 +1,32 @@
+"use strict";
+let veeva = {};
+
+let slideTrece = {
+
+   loadConfig: function() {
+      return fetch('js/config.json')
+         .then(response => response.json())
+         .then(data => {
+            veeva = data;
+         })
+         .catch(error => {
+            console.error('Error al cargar la configuración:', error);
+         });
+   },
+
+   jumptoSlide: function (slide) {
+      slide === 'refClose' && localStorage.getItem('previousSlide') ? slide = localStorage.getItem('previousSlide') : slide = '01';
+      if (typeof veeva !== 'undefined' && veeva.gotoSlide) {
+         document.location = `veeva:gotoSlide(${veeva.zipName}${slide}.zip,${veeva.presentationCode})`;
+      } else {
+         document.location = `/public/${veeva.zipName}${slide}/${veeva.zipName}${slide}.html`;
+      }
+   }
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+   slideTrece.loadConfig().then(() => {
+      console.log(`LoadConfig Ready Slide ${veeva.zipName}${veeva.slide}`);
+   });
+});
